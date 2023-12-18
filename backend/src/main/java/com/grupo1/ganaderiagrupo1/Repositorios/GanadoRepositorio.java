aed029897fc557b3d4dbbfe3ba550f21ce0e4076
wilmerxx <wmshagnay@espe.edu.ec>
2023-12-17 19:53:19 -0500

    Finalicizacion de backend

--- backend/src/main/java/com/grupo1/ganaderiagrupo1/Repositorios/GanadoRepositorio.java
+++ backend/src/main/java/com/grupo1/ganaderiagrupo1/Repositorios/GanadoRepositorio.java
@@ -6,7 +6,29 @@ import java.util.*;
 
 @Repository
 public class GanadoRepositorio {
+    Date date = new Date();
+
     List<Ganado> ganadoList = new ArrayList<>();
+    /*List<Ganado> ganadoList = new ArrayList<>(
+            List.of(
+                    new Ganado("1", "1", "1", "1", 345, "1", date, "1", "1"),
+                    new Ganado("2", "2", "2", "2", 345, "2", date, "2", "2"),
+                    new Ganado("3", "3", "3", "3", 64, "3", date, "3", "3"),
+                    new Ganado("4", "4", "4", "4", 456, "4", date, "4", "4"),
+                    new Ganado("5", "5", "5", "5", 456, "5", date, "5", "5"),
+                    new Ganado("6", "6", "6", "6", 456, "6", date, "6", "6"),
+                    new Ganado("7", "7", "7", "7", 456, "7", date, "7", "7"),
+                    new Ganado("8", "8", "8", "8", 46, "8", date, "8", "8"),
+                    new Ganado("9", "9", "9", "9", 456, "9", date, "9", "9"),
+                    new Ganado("10", "10", "10", "10", 456, "10", date, "10", "10"),
+                    new Ganado("11", "11", "11", "11", 456, "11", date, "11", "11"),
+                    new Ganado("12", "12", "12", "12", 456, "12", date, "12", "12"),
+                    new Ganado("13", "13", "13", "13", 456, "13", date, "13", "13"),
+                    new Ganado("14", "14", "14", "14", 45, "14", date, "14", "14"),
+                    new Ganado("15", "15", "15", "15", 2, "15", date, "15", "15"),
+                    new Ganado("16", "16", "16", "16", 5, "16", date, "16", "16")
+            )
+    );*/
 
     public void guardar(Object o) {
         ganadoList.add((Ganado) o);
--- backend/src/main/java/com/grupo1/ganaderiagrupo1/Repositorios/InterfazRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.grupo1.ganaderiagrupo1.Repositorios;
-
-import com.grupo1.ganaderiagrupo1.Modelos.Ganado;
-
-import java.util.List;
-
-public interface InterfazRepositorio {
-
-}
