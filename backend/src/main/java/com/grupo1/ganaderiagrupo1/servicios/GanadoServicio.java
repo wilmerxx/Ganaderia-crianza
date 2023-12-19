473fbcfa6fb4dbaa8a54feb05c186e40ee7600f6
wilmerxx <wmshagnay@espe.edu.ec>
2023-12-19 14:19:45 -0500

    cambio

--- backend/src/main/java/com/grupo1/ganaderiagrupo1/servicios/GanadoServicio.java
+++ backend/src/main/java/com/grupo1/ganaderiagrupo1/servicios/GanadoServicio.java
@@ -1,11 +1,9 @@
 package com.grupo1.ganaderiagrupo1.Servicios;
 import com.grupo1.ganaderiagrupo1.Modelos.Ganado;
-<<<<<<< HEAD
-=======
+
 
 import com.grupo1.ganaderiagrupo1.Repositorios.ControlEnfermedadesRepositorio;
 
->>>>>>> 9f232e2 (actualización del merge (#24))
 import com.grupo1.ganaderiagrupo1.Repositorios.GanadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
