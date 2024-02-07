07a840570e9d9ddbe7b9dcaf6e9b03fa676f3082
mercyarrobov <myarrobo@espe.edu.ec>
2024-02-07 03:32:55 -0500

    prueba6

--- frontend/src/app/models/inventario.ts
+++ frontend/src/app/models/inventario.ts
@@ -2,6 +2,12 @@ export class Inventario{
   id_producto?: number;
   cantidad_producto?: number;
   fecha_inventario?: string;
+  nombre_producto?: string;
+  cantidad_disponible?:string;
+  tipo?:string;
+  creado?:string;
+  modificado?:string;
+
   constructor( ) {
     this.id_producto = 0;
     this.cantidad_producto = 0;
